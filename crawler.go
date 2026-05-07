0e2bb667d8307f71102631c98c68425464d22022
Emilio Sala <emilio_k88@hotmail.com>
2026-05-07 10:15:34 +0200

    ADD: recursive crawl

--- crawler.go
+++ crawler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -35,4 +36,46 @@ func getHTML(rawURL string) (string, error) {
 		return "", err
 	}
 	return string(body), nil
+}
+
+func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
+	baseURL, err := url.Parse(rawBaseURL)
+	if err != nil {
+		return
+	}
+	currentURL, err := url.Parse(rawCurrentURL)
+	if err != nil {
+		return
+	}
+	if baseURL.Host != currentURL.Host {
+		return
+	}
+	normalizedURL, err := normalizeURL(rawCurrentURL)
+	if err != nil {
+		return
+	}
+
+	if count, ok := pages[normalizedURL]; ok {
+		pages[normalizedURL] = count + 1
+		return
+	}
+
+	pages[normalizedURL] = 1
+
+	fmt.Printf("Crawling: %s\n", rawCurrentURL)
+	html, err := getHTML(rawCurrentURL)
+	if err != nil {
+		fmt.Printf("Error getting HTML from %s: %v\n", rawCurrentURL, err)
+		return
+	}
+
+	urls, err := getURLsFromHTML(html, baseURL)
+	if err != nil {
+		fmt.Printf("Error parsing URLs from %s: %v\n", rawCurrentURL, err)
+		return
+	}
+
+	for _, nextURL := range urls {
+		crawlPage(rawBaseURL, nextURL, pages)
+	}
 }
