779470324f48964f39b020852201c84e8dc9a834
Emilio Sala <emilio_k88@hotmail.com>
2026-05-07 07:50:49 +0200

    ADD: extract links and images

--- parser.go
+++ parser.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/url"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -30,4 +31,48 @@ func getFirstParagraphFromHTML(html string) string {
 	}
 
 	return strings.TrimSpace(doc.Find("p").First().Text())
+}
+
+func getURLsFromHTML(htmlBody string, baseURL *url.URL) ([]string, error) {
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlBody))
+	if err != nil {
+		return nil, err
+	}
+
+	var urls []string
+	doc.Find("a[href]").Each(func(_ int, s *goquery.Selection) {
+		href, _ := s.Attr("href")
+		if href == "" {
+			return
+		}
+		resolvedURL, err := baseURL.Parse(href)
+		if err != nil {
+			return
+		}
+
+		urls = append(urls, resolvedURL.String())
+	})
+	return urls, nil
+}
+
+func getImagesFromHTML(htmlBody string, baseURL *url.URL) ([]string, error) {
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlBody))
+	if err != nil {
+		return nil, err
+	}
+
+	var urls []string
+	doc.Find("img[src]").Each(func(_ int, s *goquery.Selection) {
+		src, _ := s.Attr("src")
+		if src == "" {
+			return
+		}
+		resolvedURL, err := baseURL.Parse(src)
+		if err != nil {
+			return
+		}
+
+		urls = append(urls, resolvedURL.String())
+	})
+	return urls, nil
 }
